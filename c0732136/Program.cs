15000fb1dcde6b6aa41e2c6a84bdf8e2ae651b8f
visualstudio <visualstudio@122-PC32>
2019-01-25 21:20:17 -0500

    create class countryside

--- c0732136/Program.cs
+++ c0732136/Program.cs
@@ -4,12 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace c0732136
+namespace LinkedLists
 {
     class Program
     {
         static void Main(string[] args)
         {
         }
+    }
+
+    class Countryside
+    {
+        Village Alst;
+        Village Maeland;
+        Village Schenig;
+
+        public void InitializeMap()
+        {
+            Alst = new Village();
+            Maeland = new Village();
+            Maeland.isAstrildeHere = true;
+            Schenig = new Village();
+
+            Alst.NextVillage = Maeland;
+            Maeland.NextVillage = Schenig;
+            Schenig.NextVillage = Alst;
+        }
+
+        public void WalkAround()
+        {
+            Village InitialVillage = this.Maeland;
+        }
+    }
+
+    class Village
+    {
+        public Village PreviousVillage;
+        public Village NextVillage;
+        public String VillageName;
+        public bool isAstrildeHere = false;
+        public Village() { }
     }
 }
